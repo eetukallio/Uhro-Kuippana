df4e918f74af941d766692f4aaa5621aee1879c3
eetukallio <eetu.kallio@gmail.com>
2017-02-27 10:57:50 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './images/urho-logo.png';
+import './App.css';
+import Nav from './nav/Nav';
+
+class App extends Component {
+
+  render() {
+    return (
+      <Nav />
+    );
+  }
+
+
+}
+
+export default App;
