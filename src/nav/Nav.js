37fd686ac26b0f5427bc6410467a6edb0f137a4c
henri <henri.kankaanpaa@cs.tamk.fi>
2017-04-04 21:23:43 +0300

    Add login/logout buttons depending on auth state

--- src/nav/Nav.js
+++ src/nav/Nav.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import logo from '../images/urho-logo.png';
 import './Nav.css';
 import {Link} from 'react-router';
+import { connect } from 'react-redux';
+import { logout } from '../actions/actionCreators';
+// import logo from '../images/urho-logo.png';
 
 class Nav extends Component {
 
@@ -15,6 +17,11 @@ class Nav extends Component {
                         <li><Link to="/home">Tuntien syöttö </Link></li>
                         <li><Link to="/paychecks"> Palkkalaskelmat</Link> </li>
                         <li><Link to="/management">Hallinta</Link></li>
+                        {this.props.loggedIn ? (
+                                <li><a href="#" onClick={this.props.logout}>Kirjaudu ulos</a></li>
+                            ) : (
+                                <li><Link to="/login">Kirjaudu sisään</Link></li>
+                            )}
                     </ul>
                 </header>
             </div>
@@ -23,4 +30,12 @@ class Nav extends Component {
     }
 }
 
-export default Nav;
+// Which props do we want to inject, given the global state?
+function mapStateToProps(state) {
+    return {
+        loggedIn: state.loggedIn
+    }
+}
+
+// Wrap the component to inject dispatch and state into it
+export default connect(mapStateToProps, { logout })(Nav);
