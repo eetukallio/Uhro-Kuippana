f27922bc6d96c110d6372e5dbc8ac8d894dfe4cc
henri <henri.kankaanpaa@cs.tamk.fi>
2017-04-04 21:23:19 +0300

    Add proper login functionality

--- src/views/login/Login.js
+++ src/views/login/Login.js
@@ -1,69 +1,29 @@
 import React from 'react';
-import {hashHistory} from 'react-router';
+import { connect } from 'react-redux';
+import {login, changeForm} from '../../actions/actionCreators';
 import LoginForm from './form/LoginForm';
 import './Login.css'
 
 class Login extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            errors: { message: ''},
-            user: {
-                username: '',
-                password: ''
-            },
-        };
-
-        this.onChange = this.onChange.bind(this);
-        this.onSubmit = this.onSubmit.bind(this);
-    }
-
-    onChange(event) {
-        const fieldName = event.target.name;
-        console.log(fieldName);
-        const user = this.state.user;
-        user[fieldName] = event.target.value;
-
-        this.setState({
-            user
-        });
-    }
-
-    onSubmit(event) {
-        event.preventDefault();
-
-        const form = new FormData();
-        form.append("username", this.state.user.username);
-        form.append("password", this.state.user.password);
-
-        const sendData = {username: this.state.user.username, password: this.state.user.password};
-
-        console.log("ELI FORMISSA ON " + form.get("username") + form.get("password"));
-
-        var myHeaders = new Headers();
-        myHeaders.append('Content-Type', 'application/json');
-
-        fetch("http://localhost:8080/login", {
-            method: 'post',
-            headers: myHeaders,
-            mode: 'cors',
-            cache: 'default',
-            body: JSON.stringify(sendData),
-        }).then((res) => res.json()).then(data => {
-            if (data.loggedIn === true) {
-                hashHistory.push("/home");
-            }
-        }).catch(err => console.log(err))
+    login(username, password) {
+        this.props.login(username, password);
     }
 
     render() {
+        const { formState, currentlySending } = this.props.data;
         return <LoginForm
-            onSubmit={this.onSubmit}
-            onChange={this.onChange}
-            user={this.state.user}
-            errors={this.state.errors}
+            data={formState}                    onChange={this.props.changeForm}
+            location={location}                 history={this.props.history}
+            onSubmit={this.login.bind(this)}    btnText={"Login"}
+            currentlySending={currentlySending} errorMessage={this.props.data.errorMessage}
         />
     }
 }
 
-export default Login;
+function mapStateToProps(state) {
+    return {
+        data: state
+    }
+}
+
+export default connect(mapStateToProps, {login, changeForm})(Login);
