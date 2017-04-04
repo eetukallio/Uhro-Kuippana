b2756acace050ed21bb80aeceeac31f1f2ced356
henri <henri.kankaanpaa@cs.tamk.fi>
2017-04-04 21:18:29 +0300

    Create Redux store, and check if user has a cookie with correct JWT already

--- src/index.js
+++ src/index.js
@@ -1,18 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
+import App from './App';
 import Login from './views/login/Login'
 import Paychecks from './views/paychecks/Paychecks';
 import HourEntry from './views/hourentry/HourEntry';
 import Management from './views/management/Management';
+import thunk from 'redux-thunk';
+import { Router, Route, IndexRoute } from 'react-router';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { browserHistory } from 'react-router';
+import { homeReducer } from './reducers/index';
+import { checkAuth } from './utils/checkAuth';
+import cookie from 'react-cookie';
+import {SET_AUTH} from './constants/AppConstants';
 import Customers from './views/management/Customers';
 import Workers from './views/management/Workers';
 import HourEntries from './views/management/HourEntries';
 
-import { browserHistory, Router, Route, IndexRoute} from 'react-router';
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
+const store = createStoreWithMiddleware(homeReducer);
+
+const token = cookie.load('token');
+
+if (token) {
+    store.dispatch({type: SET_AUTH})
+}
 
 ReactDOM.render(
+    <Provider store={store}>
+        <Router history={browserHistory}>
+            <Route path="/" component={App}>
+                <IndexRoute component={Login}/>
+                <Route path="/login" component={Login}/>
+                <Route path="/home" component={checkAuth(HourEntry)}/>
+                <Route path="/paychecks" component={checkAuth(Paychecks)}/>
+                <Route path="/management" component={checkAuth(Management)}>
+                    <IndexRoute component={HourEntries}/>
+                    <Route path="/management/workers" component={Workers}/>
+                    <Route path="/management/customers" component={Customers}/>
+                    <Route path="/management/entries"  component={HourEntries}/>
+                </Route>
+            </Route>
+        </Router>
+    </Provider>,
     <Router history={browserHistory}>
 
         <Route path="/" component={App}>
