4c88f9113cd382a2b2e6671ddacc76a228bcaa1b
henri <henri.kankaanpaa@cs.tamk.fi>
2017-04-08 20:11:49 +0300

    Set up defaults for Axios

--- src/index.js
+++ src/index.js
@@ -18,15 +18,18 @@ import {SET_AUTH} from './constants/AppConstants';
 import Customers from './views/management/managementComponents/Customers';
 import Workers from './views/management/managementComponents/Workers';
 import HourEntries from './views/management/managementComponents/HourEntries';
+import axios from 'axios';
 
 const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 const store = createStoreWithMiddleware(homeReducer);
-
 const token = cookie.load('token');
 
 if (token) {
-    store.dispatch({type: SET_AUTH})
+    store.dispatch({type: SET_AUTH});
+    axios.defaults.headers.common['Authorization'] = token;
 }
+
+axios.defaults.baseURL = 'http://207.154.228.188:3000';
 
 ReactDOM.render(
     <Provider store={store}>
@@ -39,8 +42,8 @@ ReactDOM.render(
                 <Route path="/management" component={checkAuth(Management)}>
                     <IndexRoute component={HourEntries}/>
                     <Route path="/management/workers" component={Workers}/>
-                    <Route path="/management/customers" component={Customers}/>
-                    <Route path="/management/entries"  component={HourEntries}/>
+                    <Route path="/management/customers" component={Customers}  token={token}/>
+                    <Route path="/management/entries"  component={HourEntries}  token={token}/>
                 </Route>
             </Route>
         </Router>
