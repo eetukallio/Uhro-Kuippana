433da11b9796c76c7351f97af64beb4f09aeb356
henri <henri.kankaanpaa@cs.tamk.fi>
2017-04-11 19:20:34 +0300

    Combine the reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,36 +1,10 @@
-import { CHANGE_FORM, SET_AUTH, UNAUTH_USER, SENDING_REQUEST, SET_ERROR_MESSAGE } from '../constants/AppConstants';
+import { combineReducers } from 'redux';
+import loginReducer from './loginform_reducer';
+import hoursReducer from './hourform_reducer';
 
-const assign = Object.assign;
+const rootReducer = combineReducers({
+    auth: loginReducer,
+    hours: hoursReducer
+});
 
-// The initial application state
-const initialState = {
-    formState: {
-        username: '',
-        password: ''
-    },
-    currentlySending: false,
-    loggedIn: false,
-    errorMessage: ''
-};
-
-// Takes care of changing the application state
-export function homeReducer(state = initialState, action) {
-    switch (action.type) {
-        case CHANGE_FORM:
-            return assign({}, state, {
-                formState: action.newState
-            });
-        case SET_AUTH:
-            return {...state, errorMessage: '', formState: {username: '', password: ''}, loggedIn: true};
-        case UNAUTH_USER:
-            return {...state, loggedIn: false};
-        case SENDING_REQUEST:
-            return {...state, currentlySending: action.sending };
-        case SET_ERROR_MESSAGE:
-            return assign({}, state, {
-                errorMessage: action.message
-            });
-        default:
-            return state;
-    }
-}
+export default rootReducer;
