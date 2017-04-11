9861fa2b73d99b84f320937200ee86e239766c31
henri <henri.kankaanpaa@cs.tamk.fi>
2017-04-11 19:19:39 +0300

    Change reducer away from index.js to allow other reducers

---
+++ src/reducers/loginform_reducer.js
@@ -0,0 +1,36 @@
+import { CHANGE_FORM, SET_AUTH, UNAUTH_USER, SENDING_REQUEST, SET_ERROR_MESSAGE } from '../constants/AppConstants';
+
+const assign = Object.assign;
+
+// The initial application state
+const initialState = {
+    formState: {
+        username: '',
+        password: ''
+    },
+    loggedIn: false,
+    currentlySending: false,
+    errorMessage: ''
+};
+
+// Takes care of changing the application state
+export default function (state = initialState, action) {
+    switch (action.type) {
+        case CHANGE_FORM:
+            return assign({}, state, {
+                formState: action.newState
+            });
+        case SET_AUTH:
+            return {...state, errorMessage: '', formState: {username: '', password: ''}, loggedIn: true};
+        case UNAUTH_USER:
+            return {...state, loggedIn: false};
+        case SENDING_REQUEST:
+            return {...state, currentlySending: action.sending };
+        case SET_ERROR_MESSAGE:
+            return assign({}, state, {
+                errorMessage: action.message
+            });
+        default:
+            return state;
+    }
+}
